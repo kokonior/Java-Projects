6f4bc2c6ef1f47de37ba2f069e9d332759f24e22
ScupidC0des <11390074+GlgApr@users.noreply.github.com>
2021-10-03 11:41:12 +0700

    Learn Inheritance + GUI.

---
+++ Latihan_Inheritance2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bab4;
+
+/**
+ *
+ * @author ScupidC0des
+ */
+public class Latihan_Inheritance2 extends Latihan_Inheritance1 {
+    Latihan_Inheritance2 ()
+    {
+        nama="Kelinci";
+        biak="Vivipar";
+        nafas="Paru-paru";
+    }
+}
