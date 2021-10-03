0d3848ca44f2a0cd543be1d40191f41abeeeb298
jarkomxxx <91817006+jarkomxxx@users.noreply.github.com>
2021-10-03 12:29:00 +0700

    Create SimpleThread.java

---
+++ SimpleThread.java
@@ -0,0 +1,16 @@
+public class SimpleThread extends Thread {
+
+   public SimpleThread(String str) {
+       super(str);
+   }
+
+   public void run() {
+       for (int = 0; i < 10; i++) {
+	    System.out.println(i + " " + getName());
+	    try {
+		 sleep((int)(Math.random() * 1000));
+	    } catch (InterruptedException e) {}
+	}
+	System.out.println("DONE!" + getName());
+   }
+}
