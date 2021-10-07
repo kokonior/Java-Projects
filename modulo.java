ec7226da2a170e781a08af44f924ca3d93d68955
Wildan Fajriansyah <73598528+wfajriansyah@users.noreply.github.com>
2021-10-08 05:43:37 +0700

    Create modulo.java

---
+++ modulo.java
@@ -0,0 +1,14 @@
+class BelajarJava {
+  public static void main(String args[]){
+     
+  double a; 
+  String b;
+   
+  a = 8.0 / 3.0;
+  System.out.println("Isi variabel a: " + a); 
+   
+  b = String.format("%.2f", a);
+  System.out.println("Isi variabel b: " + b); 
+ 
+  }
+}
