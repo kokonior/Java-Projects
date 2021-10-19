92d816309d31e2a047dcd5a43dcd6e9de72b787c
ammarsufyan <ammarsufyan78@gmail.com>
2021-10-19 07:43:26 +0700

    Add Source Code

---
+++ ammarsufyan_ForWhile.java
@@ -0,0 +1,13 @@
+public class ammarsufyan_ForWhile {
+    public static void main(String[] args) {
+        for(int i = 100; i >= 0; i--) {
+            System.out.println("Hello" + " " + i);
+        }
+
+        int x = 10;
+        while(x > 0){
+            System.out.println("test " + i);
+            x--;
+        }
+    }
+}
