938c1f02df59ebf72b0565c5a1532d547d68255a
tanmyid <tanmyid1337@gmail.com>
2022-10-14 05:25:23 +0700

    Uhuyy

---
+++ BackSlash.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uhuyy;
+
+/**
+ *
+ * @author Uhuyy
+ * yang dapat saya jelaskan dari program sederhana tersebut adalah
+ * java merupakan bahasa pemprograman case sensitive, jadi harus teliti huruf besar kecil saat melakukan penulisan
+ */
+public class percobaankedua {
+    public static void main(String args[])
+{
+char bs = '\b';
+char lf = '\n';
+char tb = '\t';
+
+System.out.print("Nama Buah : ");
+// fungsi \b adalah menghapus 1 huruf dalam string
+System.out.print("1. Manggis");
+System.out.print(bs);
+//fungsi \n yaitu untuk enter atau ganti baris
+System.out.print(lf);
+System.out.print("2. Semangka");
+System.out.print("3. Durian");
+//fungsi \t adalah untuk memberi jarak pada string
+System.out.print(tb);
+System.out.print("3. Salak");
+}
+}
